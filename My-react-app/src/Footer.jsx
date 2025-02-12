aa729350c95a07103b868572d0f69a97d889aa1d
Rubeth3D <118306201+Rubeth3D@users.noreply.github.com>
2025-02-12 10:50:32 -0500

    Ajout page web REACT

---
+++ My-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <footer style={{backgroundColor: "#2C3E50", color: "white", padding: "10px", display: "flex", justifyContent: "center", alignItems: "center", position: "fixed", bottom:"0", width: "100%"}}>
+            <p>&copy; 2025 Évaluation projet</p>
+        </footer>
+    );
+};
+
+export default Footer;
