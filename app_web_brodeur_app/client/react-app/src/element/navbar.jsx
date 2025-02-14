359a1dd683caaf5fe4d3500f19b6a74f0b5c1994
cedryk Leblanc <2246462@bdeb.qc.ca>
2025-02-13 20:03:05 -0500

    placement des liens

--- app_web_brodeur_app/client/react-app/src/element/navbar.jsx
+++ app_web_brodeur_app/client/react-app/src/element/navbar.jsx
@@ -1,5 +1,31 @@
 function navbar() {
-  return <nav className="navbar navbar-light bg-light">salut</nav>;
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <a href="#" className="navbar-brand mb-0">
+        Évaluation par les pairs
+      </a>
+
+      <div className="navbar navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a href="#" className="nav-link">
+              Accueil
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a href="#" className="nav-link">
+              Cours
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a href="#" className="nav-link">
+              Connexion
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default navbar;
