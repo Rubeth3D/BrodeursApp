b1a9827dbc304d43c99950d3ee36509e38c525ba
Rubeth3D <118306201+Rubeth3D@users.noreply.github.com>
2025-02-19 10:34:30 -0500

    Ajout source dans la page Acceuil

--- My-react-app /src/Accueil.jsx
+++ My-react-app /src/Accueil.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom"; {/* Source : https://stackoverflow.com/questions/46421496/how-to-use-link-react-router-dom */}
 
 function Accueil() {
     const annee = new Date().getFullYear(); 
