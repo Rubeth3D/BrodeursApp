9158003a2b3482ab24cca656789da0a20a4ba108
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-02-22 22:21:59 -0500

    routes post et get

--- app_web_brodeur_app/serveur/main/serveur.js
+++ app_web_brodeur_app/serveur/main/serveur.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import winston from "winston";
-
+import cours from "./r"
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
@@ -18,12 +18,7 @@ const logger = winston.createLogger({
 
 function main() {
   const app = express();
-  // const con = mysql.createConnection({
-  //   host: "localhost",
-  //   user: "scott",
-  //   password: "oracle",
-  //   database: "mybd",
-  // });
+
   app.listen(8080, () => {
     logger.info("Le serveur roule sur le port 8080");
   });
--- app_web_brodeur_app/serveur/routes/cours.js
+++ app_web_brodeur_app/serveur/routes/cours.js
@@ -1,19 +1,16 @@
 import express from "express";
 import cors from "cors";
 import winston from "winston";
-import { Client } from "pg";
+import pkg from "pg";
+
+const {Client} = pkg;
 
 const client = new Client({
-  user: "placeholder",
-  password: "placeholder",
-  post: "placeholder",
+  user: "postgres",
+  password: "oracle",
+  host: "localhost",
   port: 5000,
-  database: "placeholder",
 });
-
-client.connect();
-const app = express();
-app.use(cors());
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
@@ -27,19 +24,60 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: "server.log" }),
   ],
 });
+client.connect();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+logger.info("Connecte a la bd!")
 
 app.get("/cours", async (req, res) => {
   try {
-    
-  } catch (error) {
+  const cours = await client.query("SELECT * FROM cours");
+  res.json(descriptionTables.rows);
+  } 
+  catch (error) {
     logger.error(error.message);
   }
 });
 
-app.get("/cours/:id", async (req,res) => {
+app.get("/cours/:idCours", async (req,res) => {
   try {
-    
+    const {idCours} = req.params.id;
+    const cours = await client.query("SELECT * FROM cours WHERE id_cours = $1",[id_cours]);
+    logger.info("Requete effectue avec succes!");
+    res.json(cours.rows)
   } catch (error) {
     logger.error(error);
   }
 })
+
+app.post("/cours", async (req,res) =>{
+  try {
+    const { id_cours, code_cours, description_cours, etat_cours, session_id_session } = req.body;    
+    logger.info(req.body.rows);
+    const nouveauCours = await client.query("INSERT INTO cours (id_cours, code_cours, description_cours, etat_cours, session_id_session) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [id_cours, code_cours, description_cours, etat_cours, session_id_session]
+    );
+    
+    logger.info("Insert effectue avec succes!");
+    logger.info(`Nouveau Cours : ${requete.rows}`);
+  } catch (error) {
+    logger.error(error);
+  }
+})
+
+app.put("/cours/:idCours", async (req,res) =>{
+  try{
+    const {idCours} = req.params.id;
+    const {code_cours, description_cours, etat_cours, session_id_session } = req.body;
+  }catch(error){
+    logger.error(err);
+  }
+});
+
+
+app.listen(3000, () => {
+  logger.info("Le serveur roule sur le port 3000");
+});
