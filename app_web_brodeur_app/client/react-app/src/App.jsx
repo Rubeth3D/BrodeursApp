e591c253e6e7860a3e154d12c6f39e0fbd26819f
cedryk Leblanc <2246462@bdeb.qc.ca>
2025-02-23 22:43:44 -0500

    page créer un compte

--- app_web_brodeur_app/client/react-app/src/App.jsx
+++ app_web_brodeur_app/client/react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./pages/Home/Home.jsx";
 import Connexion from "./pages/Connexion/Connexion.jsx";
+import CreateAccount from "./pages/CreateAccount/CreateAccount.jsx";
 
 function App() {
   return (
     <>
-      <Connexion></Connexion>
+     <CreateAccount></CreateAccount>
     </>
   );
 }
