2c91d620c6d5e68e91bb2bee1ad101d83ad01723
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-02-24 16:55:33 -0500

    finition de la route cours

--- app_web_brodeur_app/serveur/routes/cours.js
+++ app_web_brodeur_app/serveur/routes/cours.js
@@ -1,16 +1,9 @@
 import express from "express";
 import cors from "cors";
 import winston from "winston";
-import pkg from "pg";
+import client from "../bd/connexion.js";
 
-const {Client} = pkg;
-
-const client = new Client({
-  user: "postgres",
-  password: "oracle",
-  host: "localhost",
-  port: 5000,
-});
+//logger winston setup
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
@@ -24,60 +17,106 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: "server.log" }),
   ],
 });
-client.connect();
 
-const app = express();
+const { Routeur } = express();
 
-app.use(cors());
-app.use(express.json());
-logger.info("Connecte a la bd!")
+Routeur.use(cors());
+Routeur.use(express());
 
-app.get("/cours", async (req, res) => {
+//get en tableau
+Routeur.get("/cours", async (req, res) => {
   try {
-  const cours = await client.query("SELECT * FROM cours");
-  res.json(descriptionTables.rows);
-  } 
-  catch (error) {
+    const cours = await client.query("SELECT * FROM cours");
+    logger.info("Get des cours effectue avec succes");
+    res.json(descriptionTables.rows);
+    res.status(res.StatusCode);
+  } catch (error) {
     logger.error(error.message);
+    res
+      .status(res.StatusCode)
+      .json({ message: `Il y a eu une erreur de type ${res.StatusCode}` });
   }
 });
-
-app.get("/cours/:idCours", async (req,res) => {
+//get avec le id
+Routeur.get("/cours/:idCours", async (req, res) => {
   try {
-    const {idCours} = req.params.id;
-    const cours = await client.query("SELECT * FROM cours WHERE id_cours = $1",[id_cours]);
-    logger.info("Requete effectue avec succes!");
-    res.json(cours.rows)
+    const { idCours } = req.params.id;
+    const cours = await client.query(
+      "SELECT * FROM cours WHERE id_cours = $1",
+      [id_cours]
+    );
+    logger.info("Get du cours effectue avec succes");
+    res.json(cours.rows);
+    res.status(res.StatusCode);
   } catch (error) {
     logger.error(error);
+    res.status(res.StatusCode);
   }
-})
-
-app.post("/cours", async (req,res) =>{
+});
+//post un cours
+Routeur.post("/cours", async (req, res) => {
   try {
-    const { id_cours, code_cours, description_cours, etat_cours, session_id_session } = req.body;    
+    const {
+      id_cours,
+      code_cours,
+      description_cours,
+      etat_cours,
+      session_id_session,
+    } = req.body;
     logger.info(req.body.rows);
-    const nouveauCours = await client.query("INSERT INTO cours (id_cours, code_cours, description_cours, etat_cours, session_id_session) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    const nouveauCours = await client.query(
+      "INSERT INTO cours (id_cours, code_cours, description_cours, etat_cours, session_id_session) VALUES ($1, $2, $3, $4, $5) RETURNING *",
       [id_cours, code_cours, description_cours, etat_cours, session_id_session]
     );
-    
-    logger.info("Insert effectue avec succes!");
+
+    logger.info("Insert effectue avec succes");
     logger.info(`Nouveau Cours : ${requete.rows}`);
+    res.status(res.StatusCode).json({ message: "Cours inserre avec succes!" });
   } catch (error) {
     logger.error(error);
-  }
-})
-
-app.put("/cours/:idCours", async (req,res) =>{
-  try{
-    const {idCours} = req.params.id;
-    const {code_cours, description_cours, etat_cours, session_id_session } = req.body;
-  }catch(error){
-    logger.error(err);
+    res
+      .status(res.StatusCode)
+      .json({ message: `Il y a eu une erreur de type ${res.StatusCode}` });
   }
 });
 
+//update un cours
+Routeur.put("/cours/:idCours", async (req, res) => {
+  try {
+    const { idCours } = req.params.id;
+    const { code_cours, description_cours, etat_cours, session_id_session } =
+      req.body;
+    const coursUpdate = client.query(
+      "UPDATE cours SET code_cours = 1$, description_cours = 2$, etat_cours = 3$, session_id_session = 4$ WHERE id_cours = $5",
+      [code_cours, description_cours, etat_cours, session_id_session, idCours]
+    );
+    res
+      .status(res.StatusCode)
+      .json({ message: "Cours mis a jour avec succes !" });
+  } catch (err) {
+    logger.error(err);
+    res
+      .status(res.StatusCode)
+      .json({ message: `Il y a eu une erreur de type ${res.StatusCode}` });
+  }
+});
 
-app.listen(3000, () => {
+//delete un cours
+Routeur.delete("/cours/:idCours", async (req, res) => {
+  try {
+    const idCours = req.params.id;
+    const coursDelete = client.query("DELETE cours WHERE id = $1", [idCours]);
+    res.status().json({ message: "Cours mis a jour avec succes !" });
+  } catch (err) {
+    logger.info(err);
+    res
+      .status(res.StatusCode)
+      .json({ message: `Il y a eu une erreur de type ${res.StatusCode}` });
+  }
+});
+
+Routeur.listen(3000, () => {
   logger.info("Le serveur roule sur le port 3000");
 });
+
+export default Routeur;
