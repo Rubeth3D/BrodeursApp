33d549309f18600fad85b87888b1088963178596
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-02-27 22:33:38 -0500

    modif

--- app_web_brodeur_app/Client/react-app/src/pages/Cours/Cours.jsx
+++ app_web_brodeur_app/Client/react-app/src/pages/Cours/Cours.jsx
@@ -1,61 +1,78 @@
 import Navbar from "../../element/navbar";
 import Footer from "../../element/footer";
 import { Link } from "react-router-dom";
-import react ,{ useEffect, useState} from "react";
-import { Fragment} from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 function Cours() {
-  // const [dataCours, setDataCours] = useState({
-  //   id_cours : "",
-  //   code_cours : "",
-  //   description_cours : "",
-  //   etat_cours : "",
-  //   session_id_session : ""
-  // });
-  // const onSubmitForm = async() => {
-  //   try {
-  //     const body = {dataCours};
-  //     const reponse = ("http://localhost:");
-  //   } catch (err) {
-  //     console.error(`ERREUR CLIENT COURS FETCH ${err}`);
-  //   }
-  // }
+  const [cours, setCours] = useState([]);
+  //fonction pour delete un cours
+  const deleteCours = async (id_cours) => {
+    try {
+      const deleteCours = await fetch(
+        `http://localhost:8080/cours/${id_cours}`,
+        {
+          method: "DELETE",
+        }
+      );
+      setCours(cours.filter((cours) => cours.id_cours !== id_cours));
+    } catch (err) {
+      console.log(`Erreur lors du delete du cours : ${err}`);
+    }
+  };
+  //fonction pour get les cours
   const getCours = async () => {
     try {
       const reponse = await fetch("http://localhost:8080/cours");
       const jsonData = await reponse.json();
       console.log(jsonData);
+      setCours(jsonData);
     } catch (err) {
-      console.error(err);
+      console.error(`Erreur lors du fetch des cours : ${err}`);
+    }
+  };
+  const updateCours = async (id_cours) =>{
+    try {
+      const reponse = await fetch("")
+    } catch (err) {
+      console.log(`Erreur lors du update du cours ${err}`);
     }
   }
   useEffect(() => {
     getCours();
-  })
+  }, []);
   return (
     <>
       <Navbar />
-            <Fragment>
-                <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">id</th>
-                  <th scope="col">Code du cours</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Etat du cours</th>
-                  <th scope="col">Id de session</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* {<tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>} */}
-              </tbody>
-            </table>
-            </Fragment>
+      <Fragment>
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Code du cours</th>
+              <th scope="col">Description</th>
+              <th scope="col">Etat du cours</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cours.map((cours) => (
+              <tr key={cours.id_cours}>
+                <td>{cours.code_cours}</td>
+                <td>{cours.description_cours}</td>
+                <td>{cours.etat_cours}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteCours(cours.id_cours)}
+                  >
+                    delete
+                  </button>
+                  <button className="btn btn-primary">update</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
       <Footer />
     </>
   );
