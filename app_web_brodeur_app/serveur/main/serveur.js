376780b400804a2daf8130546b17cf0b2b95418b
cedryk Leblanc <2246462@bdeb.qc.ca>
2025-03-09 13:07:22 -0400

    route get les sessions

--- app_web_brodeur_app/serveur/main/serveur.js
+++ app_web_brodeur_app/serveur/main/serveur.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import winston from "winston";
 import cours from "../routes/cours.js";
 import utilisateur from "../routes/utilisateur.js";
+import session from "../routes/session.js";
+
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
@@ -21,6 +23,7 @@ const app = express();
 app.use(cors());
 app.use("/cours", cours);
 app.use("/utilisateur", utilisateur);
+app.use("/session", session);
 app.listen(8080, () => {
   logger.info("Le serveur roule sur le port 8080");
 });
---
+++ app_web_brodeur_app/serveur/routes/session.js
@@ -0,0 +1,36 @@
+import express, { json, query } from "express";
+import cors from "cors";
+import winston from "winston";
+import client from "../bd/connexion.js";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "server.log" }),
+  ],
+});
+
+const router = express.Router();
+
+router.use(express.json());
+
+//get pour les sessions
+router.get("/", async (req, res) => {
+  try {
+    const resultat = await client.query("SELECT * FROM session");
+    res.status(200).json(resultat.rows);
+    logger.info("Get des sessions effectuer avec succes!");
+  } catch (err) {
+    logger.error(`Erreur lors du fetch des sessions : ${err}`);
+    res.status(500).json({ message: "Erreur lors du fetch des sessions!" });
+  }
+});
+
+export default router;
