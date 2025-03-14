1388cc8bd2313f3e25d8c69938ea150c45cc36b7
Rubeth3D <118306201+Rubeth3D@users.noreply.github.com>
2025-03-13 21:36:08 -0400

    Backend de la classe

--- app_web_brodeur_app/serveur/routes/classe.js
+++ app_web_brodeur_app/serveur/routes/classe.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import winston from "winston";
+import winston, { log } from "winston";
 import client from "../bd/connexion.js";
 
 const logger = winston.createLogger({
@@ -68,4 +68,71 @@ router.post("/", async (req, res) => {
     const { codeCours, description, session, groupe, etat_classe} = req.params;
     const resultat = await client.query("INSERT");
   } catch (err) {}
+});
+
+//Insert d'une classe
+router.post("/", async (req, res) => {
+  try {
+    const {code_cours,description,groupe,professeur_id_professeur,cours_id_cours,etat_classe} = req.body;
+    const resultat = await client.query(
+      "INSERT INTO classe (code_cours,description,groupe,professeur_id_professeur,cours_id_cours,etat_classe) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *",
+      [code_cours,description,groupe,professeur_id_professeur,cours_id_cours,etat_classe]);
+      logger.info("Insertion de la classe effectue avec succes!");
+      res.status(201).json(resultat.rows[0]);
+  } catch (err) {
+    logger.error(`Erreur lors de l'insertion de la classe ${err}`);
+    res.status(500);
+    res.json({
+      message: "Erreur lors de l'insertion de la classe",
+    });
+  }
+});
+
+//Update d'une classe
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params;
+    const {code_cours,description,groupe,professeur_id_professeur,cours_id_cours,etat_classe} = req.body;
+    const resultat = await client.query(
+      "UPDATE classe SET code_cours = $1, description = $2, groupe = $3, professeur_id_professeur = $4, cours_id_cours = $5, etat_classe = $6 WHERE id_classe = $7 RETURNING *",
+      [code_cours,description,groupe,professeur_id_professeur,cours_id_cours,etat_classe,id]
+    );
+    if (resultat.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ message: `Aucune classe ne correspond au id : ${id}!` });
+    }
+    logger.info("Update de la classe effectue avec succes!");
+    res.status(200).json(resultat.rows[0]);
+  } catch (err) {
+    logger.error(`Erreur lors de l'update de la classe ${err}`);
+    res.status(500);
+    res.json({
+      message: "Erreur lors de l'update de la classe",
+    });
+  }
+});
+
+//Delete d'une classe
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params;
+    const resultat = await client.query(
+      "DELETE FROM classe WHERE id_classe = $1 RETURNING *",
+      id
+    );
+    if (resultat.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ message: `Aucune classe ne correspond au id : ${id}!` });
+    }
+    logger.info("Delete de la classe effectue avec succes!");
+    res.status(200).json(resultat.rows[0]);
+  } catch (err) {
+    logger.error(`Erreur lors du delete de la classe ${err}`);
+    res.status(500);
+    res.json({
+      message: "Erreur lors du delete de la classe",
+    });
+  }
 });
