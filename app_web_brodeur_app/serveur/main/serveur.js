d8e64daf9e589c75372fc08ca1ebfaca67191dd9
cedryk Leblanc <2246462@bdeb.qc.ca>
2025-03-19 14:43:51 -0400

    nouvelle route

--- app_web_brodeur_app/serveur/main/serveur.js
+++ app_web_brodeur_app/serveur/main/serveur.js
@@ -5,6 +5,7 @@ import winston from "winston";
 import cours from "../routes/cours.js";
 import utilisateur from "../routes/utilisateur.js";
 import session from "../routes/session.js";
+import verificationCookie from "../routes/verify-cookie.js";
 
 const logger = winston.createLogger({
   level: "info",
@@ -31,6 +32,7 @@ app.use(cookieParser());
 app.use("/cours", cours);
 app.use("/utilisateur", utilisateur);
 app.use("/session", session);
+app.use("/verify-cookie", verificationCookie);
 app.listen(8080, () => {
   logger.info("Le serveur roule sur le port 8080");
 });
---
+++ app_web_brodeur_app/serveur/routes/verify-cookie.js
@@ -0,0 +1,25 @@
+import express, { json, query } from "express";
+import cors from "cors";
+import winston from "winston";
+import client from "../bd/connexion.js";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "server.log" }),
+  ],
+});
+
+const router = express.Router();
+
+router.use(express.json());
+
+//Route protégé
+router.get("/veryfy-cookie")
