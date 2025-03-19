6f28f7f43f176af343c4d993564cde7a3dfd4e18
Rubeth3D <118306201+Rubeth3D@users.noreply.github.com>
2025-03-19 09:21:18 -0400

    L'interface classe

--- app_web_brodeur_app/client/react-app/src/pages/Classe/classe.jsx
+++ app_web_brodeur_app/client/react-app/src/pages/Classe/classe.jsx
@@ -8,10 +8,16 @@ function Classe() {
         <>
         <Navbar/>
         <div className="container mt-5">
+            <h1 className="text-center">Liste des classes</h1>
+        </div>
+        <div className="container mt-5">
             <div className="mt-3">
-                <input type="text" className="form-control fs-5" />
-                <h2>Hello world</h2>
+                <input type="text" className="form-control" placeholder="Rechercher une classe" />
             </div>
+        </div>
+
+        <div className="d-flex justify-content-end mt-5 mx-5" >
+            <button type="button" className="btn btn-primary"> + Ajouter une classe</button>
         </div>
         <Footer/>
         </>
