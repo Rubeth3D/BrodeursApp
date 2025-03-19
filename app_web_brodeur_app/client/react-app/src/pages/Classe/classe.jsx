6fefedad05c45287b5b3cbc6a0c76f3aeab1a802
Rubeth3D <118306201+Rubeth3D@users.noreply.github.com>
2025-03-19 08:54:40 -0400

    Supprimer l'import Bootstrap

--- app_web_brodeur_app/client/react-app/src/pages/Classe/classe.jsx
+++ app_web_brodeur_app/client/react-app/src/pages/Classe/classe.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import bootstrap from "bootstrap";
 import Navbar from "../../element/navbar";
 import Footer from "../../element/footer";
 import { Link } from "react-router-dom";
@@ -11,8 +10,10 @@ function Classe() {
         <div className="container mt-5">
             <div className="mt-3">
                 <input type="text" className="form-control fs-5" />
+                <h2>Hello world</h2>
             </div>
         </div>
+        <Footer/>
         </>
     ); 
 }
