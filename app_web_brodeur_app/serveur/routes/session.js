1b24f6ab0f2f3e3bc2ea2893871c752196f25bb6
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-03-26 15:56:15 -0400

    commit

--- app_web_brodeur_app/serveur/routes/session.js
+++ app_web_brodeur_app/serveur/routes/session.js
@@ -33,5 +33,11 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: "Erreur lors du fetch des sessions!" });
   }
 });
-router.post("/", async (req,res) =>)
+router.post("/", async (req, res) => {
+  try {
+    const { code_session, date_session, etat_professeur, utilisateur_id_user } =
+      req.body;
+    const resultat = await connexionPostgres.query();
+  } catch (error) {}
+});
 export default router;
