deb5486e9f723eb1c52e9d66aff859fb7c86a5c9
Rubeth3D <118306201+Rubeth3D@users.noreply.github.com>
2025-03-26 13:47:49 -0400

    Constante d'équipe dans l'élément d'équipe

--- app_web_brodeur_app/client/react-app/src/element/Equipe.jsx
+++ app_web_brodeur_app/client/react-app/src/element/Equipe.jsx
@@ -1,5 +1,74 @@
+import e from "express";
 import React, { useState, useEffect, useRef } from "react";
 
-const Equipe = () => {};
+const Equipe = () => {
+    const urlEquipe = "http://localhost:8080/equipe";
+    const urlClasse = "http://localhost:8080/classe";
+    const estFetchedClasse = useRef(false);
+    const [classe, setClasse] = useState([]);
+    const [classeSelected, setClasseSelect] = useState("");
+    const [equipe, nouvelleEquipe] = useState({
+        code_equipe: "",
+        nom: "",
+        etat_equipe: "A",
+    });
+
+    const GetClasse = async () => {
+        try {
+            const reponse = await fetch(urlClasse);
+            const objetClasse = await reponse.json();
+            setClasse(objetClasse);
+        } catch (err) {
+            console.log(`Erreur lors du fetch des classes : ${err}`);
+        }
+    };
+
+    const postEquipe = async (e, equipe, classe) => {
+        e.preventDefault();
+        try{
+           console.log(equipe);
+              const data = {
+                code_equipe: equipe.code_equipe,
+                nom: equipe.nom,
+                etat_equipe: equipe.etat_equipe,
+                classe_id_classe: classe,
+            };
+            console.log(data);
+            const reponse = await fetch(`${urlEquipe}`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data),
+            });
+            if (!reponse.ok) {
+                throw new Error("Erreur lors de la mise a jour de l'equipe");
+            }
+            window.location.reload();
+            console.log("Equipe insere avec succes");
+        } catch (err) {
+            console.log(`Erreur lors de l'insertion de l'equipe ${err}`);
+        }
+    };
+
+    const modifierEquipe = (e) => {
+        const {name,value} = e.target;
+        setEquipeMisAJour((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    useEffect(() => {
+        if (!estFetchedClasse.current) {
+            GetClasse();
+            estFetchedClasse.current = true;
+        }
+    }, []);
+
+    return(
+        <>
+        
+        </>
+    )
+};
 
 export default Equipe;
