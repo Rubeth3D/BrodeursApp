01584ac0c2fccd632cc2947c92f65e93896868e9
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-03-26 17:59:27 -0400

    hover finished

--- app_web_brodeur_app/Client/react-app/src/element/HoverDiv.jsx
+++ app_web_brodeur_app/Client/react-app/src/element/HoverDiv.jsx
@@ -3,15 +3,18 @@ import React, { useState } from "react";
 function HoverDiv({ text, svgImage }) {
   // État pour gérer la couleur de la div au survol
   const [isHovered, setIsHovered] = useState(false);
+  const [opacity, setOpacity] = useState(1);
 
   // Changer la couleur de fond lors du survol
   const handleMouseOver = () => {
     setIsHovered(true);
+    setOpacity(0.8);
   };
 
   // Rétablir la couleur de fond lors du retrait du survol
   const handleMouseOut = () => {
     setIsHovered(false);
+    setOpacity(1);
   };
 
   return (
@@ -21,18 +24,19 @@ function HoverDiv({ text, svgImage }) {
       style={{
         display: "flex",
         alignItems: "center", // Aligne verticalement l'image et le texte
-        padding: "10px",
+        padding: "8px",
+        margin: "0.75rem",
+        opacity: opacity,
         backgroundColor: isHovered ? "#0d6efd" : "transparent", // Change la couleur de fond en bleu au survol
         borderRadius: "8px", // Ajoute un rayon pour arrondir les coins de la div
         cursor: "pointer", // Change le curseur en pointeur lorsqu'on survole
         transition: "background-color 0.3s ease", // Animation de transition lors du changement de couleur
       }}
     >
-      <div style={{ marginRight: "10px" }}>
-        {/* Affiche l'image SVG */}
+      <div style={{ marginRight: "1rem",color: isHovered ? "white" : "black"  }}>
         {svgImage}
       </div>
-      <div style={{ fontSize: "18px", color: isHovered ? "white" : "black" }}>
+      <div style={{ fontSize: "20px", color: isHovered ? "white" : "black" }}>
         {/* Affiche le texte */}
         {text}
       </div>
