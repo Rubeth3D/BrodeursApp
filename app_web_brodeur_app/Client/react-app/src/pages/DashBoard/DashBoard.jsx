0827c66fa500b8c21e29fee806504a1f25d3d8b7
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-03-31 17:51:58 -0400

    align a gauche

--- app_web_brodeur_app/Client/react-app/src/pages/DashBoard/DashBoard.jsx
+++ app_web_brodeur_app/Client/react-app/src/pages/DashBoard/DashBoard.jsx
@@ -1,9 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import React, { useRef, useState } from "react";
-import HoverText from "../../element/HoverText";
 import HoverDiv from "../../element/HoverDiv";
 import ActiviteSVG from "../../image/ActiviteSVG";
-import Admin from "../../image/AdminSVG";
 import AssignationsSVG from "../../image/AssignationsSVG";
 import ClassesSVG from "../../image/ClassesSVG";
 import CoursSVG from "../../image/CoursSVG";
@@ -40,74 +38,81 @@ function DashBoard() {
       <h2 className="text-primary mx-3 mt-5 fw-normal">
         Évaluation par les pairs
       </h2>
-      <div
-        className="justify-content-start mt-5 mx-3"
-        style={{ width: "12rem" }}
-      >
-        <HoverDiv
-          text={"Dashboard"}
-          svgImage={<DashBoardSVG />}
-          isCliquer={etatBoutton[0].isActiver}
-          onclick={() => {
-            Cliquer("Dashboard");
-          }}
-        ></HoverDiv>
-        <HoverDiv
-          text={"Cours"}
-          svgImage={<CoursSVG />}
-          isCliquer={etatBoutton[1].isActiver}
-          onclick={() => {
-            Cliquer("Cours");
-          }}
-        ></HoverDiv>
-        <HoverDiv
-          text={"Classes"}
-          svgImage={<ClassesSVG />}
-          isCliquer={etatBoutton[2].isActiver}
-          onclick={() => {
-            Cliquer("Classes");
-          }}
-        ></HoverDiv>
-        <HoverDiv
-          text={"Equipes"}
-          svgImage={<EquipeSVG />}
-          isCliquer={etatBoutton[3].isActiver}
-          onclick={() => {
-            Cliquer("Equipes");
-          }}
-        ></HoverDiv>
-        <HoverDiv
-          text={"Activité"}
-          svgImage={<ActiviteSVG />}
-          isCliquer={etatBoutton[4].isActiver}
-          onclick={() => {
-            Cliquer("Activité");
-          }}
-        ></HoverDiv>
-        <HoverDiv
-          text={"Assignations"}
-          svgImage={<AssignationsSVG />}
-          isCliquer={etatBoutton[5].isActiver}
-          onclick={() => {
-            Cliquer("Assignations");
-          }}
-        ></HoverDiv>
-        <HoverDiv
-          text={"Resultats"}
-          svgImage={<ResultatsSVG />}
-          isCliquer={etatBoutton[6].isActiver}
-          onclick={() => {
-            Cliquer("Resultats");
-          }}
-        ></HoverDiv>
-        <HoverDiv
-          text={"Admin"}
-          svgImage={<AdminSVG />}
-          isCliquer={etatBoutton[7].isActiver}
-          onclick={() => {
-            Cliquer("Admin");
-          }}
-        ></HoverDiv>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-2 ms-0 d-flex justify-content-start">
+            <div
+              className="justify-content-start mt-5 mx-3 col-6"
+              style={{ width: "12rem" }}
+            >
+              <HoverDiv
+                text={"Dashboard"}
+                svgImage={<DashBoardSVG />}
+                isCliquer={etatBoutton[0].isActiver}
+                onclick={() => {
+                  Cliquer("Dashboard");
+                }}
+              ></HoverDiv>
+              <HoverDiv
+                text={"Cours"}
+                svgImage={<CoursSVG />}
+                isCliquer={etatBoutton[1].isActiver}
+                onclick={() => {
+                  Cliquer("Cours");
+                }}
+              ></HoverDiv>
+              <HoverDiv
+                text={"Classes"}
+                svgImage={<ClassesSVG />}
+                isCliquer={etatBoutton[2].isActiver}
+                onclick={() => {
+                  Cliquer("Classes");
+                }}
+              ></HoverDiv>
+              <HoverDiv
+                text={"Equipes"}
+                svgImage={<EquipeSVG />}
+                isCliquer={etatBoutton[3].isActiver}
+                onclick={() => {
+                  Cliquer("Equipes");
+                }}
+              ></HoverDiv>
+              <HoverDiv
+                text={"Activité"}
+                svgImage={<ActiviteSVG />}
+                isCliquer={etatBoutton[4].isActiver}
+                onclick={() => {
+                  Cliquer("Activité");
+                }}
+              ></HoverDiv>
+              <HoverDiv
+                text={"Assignations"}
+                svgImage={<AssignationsSVG />}
+                isCliquer={etatBoutton[5].isActiver}
+                onclick={() => {
+                  Cliquer("Assignations");
+                }}
+              ></HoverDiv>
+              <HoverDiv
+                text={"Resultats"}
+                svgImage={<ResultatsSVG />}
+                isCliquer={etatBoutton[6].isActiver}
+                onclick={() => {
+                  Cliquer("Resultats");
+                }}
+              ></HoverDiv>
+              <HoverDiv
+                text={"Admin"}
+                svgImage={<AdminSVG />}
+                isCliquer={etatBoutton[7].isActiver}
+                onclick={() => {
+                  Cliquer("Admin");
+                }}
+              ></HoverDiv>
+            </div>
+          </div>
+          <div className="col-10"></div>
+        </div>
       </div>
     </>
   );
