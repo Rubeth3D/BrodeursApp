9bf6beea8b13e4231c85126675ba80d46006902d
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-04-09 09:32:39 -0400

    e

---
+++ app_web_brodeur_app/Client/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./react-app/src/App";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
