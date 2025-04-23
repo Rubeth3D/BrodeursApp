e297f477559c32b0f77fe79ccf2ea01b1b3b303e
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-04-23 10:37:31 -0400

    Update local-strategy.mjs

--- app_web_brodeur_app/serveur/strategies/local-strategy.mjs
+++ app_web_brodeur_app/serveur/strategies/local-strategy.mjs
@@ -7,25 +7,24 @@ async function generateUniqueSessionId(client) {
   let exists = true;
 
   while (exists) {
-    id = Math.floor(100000 + Math.random() * 900000);  
+    id = Math.floor(100000 + Math.random() * 900000);
     const result = await client.query(
-      'SELECT 1 FROM session_utilisateur WHERE id_session_utilisateur = $1',
+      "SELECT 1 FROM session_utilisateur WHERE id_session_utilisateur = $1",
       [id]
-    );  
+    );
 
-    exists = result.rowCount > 0;  
+    exists = result.rowCount > 0;
   }
 
-  return id; 
+  return id;
 }
 
 export default passport.use(
-  new LocalStrategy(async (username, password, done) => {  
+  new LocalStrategy(async (username, password, done) => {
     const requete = "SELECT * FROM utilisateur WHERE nom_utilisateur = $1;";
     const parametre = [username];
 
     try {
-      
       const result = await client.query(requete, parametre);
 
       if (result.rows.length === 0) {
@@ -35,28 +34,32 @@ export default passport.use(
         });
       }
 
-      const utilisateur = result.rows[0];  
+      const utilisateur = result.rows[0];
 
-      
-      if (username === utilisateur.nom_utilisateur && password === utilisateur.mot_passe) {
+      if (
+        username === utilisateur.nom_utilisateur &&
+        password === utilisateur.mot_passe
+      ) {
         console.log("Mot de passe et Nom d'utilisateur Valide");
 
-       
-        const sessionId = await generateUniqueSessionId(client);  
+        const sessionId = await generateUniqueSessionId(client);
 
         const utilisateurId = utilisateur.id_utilisateur;
-        const dateConnexion = new Date().toISOString(); 
-        const dateJetonExpiration = new Date(Date.now() + 60 * 60 * 1000).toISOString();
+        const dateConnexion = new Date().toISOString();
+        const dateJetonExpiration = new Date(
+          Date.now() + 60 * 60 * 1000
+        ).toISOString();
         const typeUtilisateur = utilisateur.type_utilisateur;
         const etatSession = "A";
 
-        
         const regarderSessionQuery = `
           SELECT * FROM session_utilisateur 
           WHERE utilisateur_id_utilisateur = $1 AND etat_session_utilisateur = 'A';
         `;
 
-        const sessionCheckResult = await client.query(regarderSessionQuery, [utilisateurId]); 
+        const sessionCheckResult = await client.query(regarderSessionQuery, [
+          utilisateurId,
+        ]);
 
         if (sessionCheckResult.rows.length > 0) {
           console.log("Session Active trouver");
@@ -73,11 +76,15 @@ export default passport.use(
 
           const updateParams = [dateConnexion, dateJetonExpiration, sessionId];
 
-          const updatedSessionResult = await client.query(updateSessionQuery, updateParams); 
+          const updatedSessionResult = await client.query(
+            updateSessionQuery,
+            updateParams
+          );
           console.log("Update de la session");
 
-          utilisateur.session_id = updatedSessionResult.rows[0].id_session_utilisateur;
-          return done(null, utilisateur);  
+          utilisateur.session_id =
+            updatedSessionResult.rows[0].id_session_utilisateur;
+          return done(null, utilisateur);
         } else {
           console.log("Aucune session existante ou active");
 
@@ -92,13 +99,20 @@ export default passport.use(
             ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id_session_utilisateur;
           `;
 
-          const params = [sessionId, dateConnexion, dateJetonExpiration, typeUtilisateur, utilisateurId, etatSession];
+          const params = [
+            sessionId,
+            dateConnexion,
+            dateJetonExpiration,
+            typeUtilisateur,
+            utilisateurId,
+            etatSession,
+          ];
 
-          const sessionResult = await client.query(insertSessionQuery, params);  
+          const sessionResult = await client.query(insertSessionQuery, params);
           const newSessionId = sessionResult.rows[0].id_session_utilisateur;
 
           utilisateur.session_id = newSessionId;
-          return done(null, utilisateur);  
+          return done(null, utilisateur);
         }
       } else {
         console.log("BRUHHH so close");
@@ -107,14 +121,14 @@ export default passport.use(
         });
       }
     } catch (err) {
-      console.error("Erreur lors de la requête :", err);  
-      return done(err);  
+      console.error("Erreur lors de la requête :", err);
+      return done(err);
     }
   })
 );
 
 passport.serializeUser((user, done) => {
-  done(null, user.id_utilisateur);  
+  done(null, user.id_utilisateur);
 });
 
 passport.deserializeUser((id, done) => {
@@ -123,6 +137,6 @@ passport.deserializeUser((id, done) => {
   client.query(requete, [id], async (err, result) => {
     if (err) return done(err);
     if (result.rows.length === 0) return done(null, false);
-    return done(null, result.rows[0]);  
+    return done(null, result.rows[0]);
   });
 });
