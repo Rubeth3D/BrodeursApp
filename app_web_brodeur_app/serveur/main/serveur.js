49c7f874100ebe125139e027d13da270e73fe4b8
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-05-07 11:35:58 -0400

    Connexion marche

--- app_web_brodeur_app/serveur/main/serveur.js
+++ app_web_brodeur_app/serveur/main/serveur.js
@@ -1,22 +1,20 @@
-import express from "express";
+import express, { request, response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import winston from "winston";
 import cours from "../routes/cours.js";
-import utilisateur from "../routes/utilisateur.js";
-import session from "../routes/sessionCours.js";
-import historiqueDesSessions from "../routes/HistoriqueDesSessions.js";
-import inscription from "../routes/inscription.js";
-import passport from "passport";
-import sessionExpress from "express-session";
-import { config } from "dotenv";
-import initialize from "../strategies/local-strategy.mjs";
-import client from "../bd/postgresBD/Connexion.js";
 import classe from "../routes/classe.js";
-import equipe from "../routes/equipe.js";
+import utilisateur from "../routes/utilisateur.js";
 import etudiant from "../routes/etudiant.js";
-import etudiantEquipe from "../routes/etudiantEquipe.js";
-import sessionCours from "../routes/sessionCours.js";
+import sessionDeCours from "../routes/sessionCours.js";
+import connexion from "../routes/connexion.js";
+import passport from "passport";
+import session from "express-session";
+import "./../strategies/local-strategy.mjs";
+import HistoriqueSession from "../routes/HistoriqueDesSessions.js";
+import Commentaire from "../routes/commentaire.js";
+
+const app = express();
 
 const logger = winston.createLogger({
   level: "info",
@@ -35,25 +33,33 @@ const corsConfig = {
   credentials: true,
   origin: true,
 };
-config();
-const app = express();
 
-app.use(express.json());
+app.use(
+  session({
+    secret: "BrodeurApps",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 24,
+    },
+  })
+);
 
-//initialisation du passport
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors(corsConfig));
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/cours", cours);
 app.use("/utilisateur", utilisateur);
-app.use("/sessionCours", session);
-app.use("/logSessions", historiqueDesSessions);
+app.use("/sessionCours", sessionDeCours);
 app.use("/classe", classe);
-app.use("/equipe", equipe);
-app.use("/session", session);
 app.use("/etudiant", etudiant);
-app.use("/etudiantEquipe", etudiantEquipe);
-app.use("/inscription", inscription);
-app.use("/sessionCours", sessionCours);
+app.use("/historiqueDesSessions", HistoriqueSession);
+app.use("/commentaire", Commentaire);
+app.use("/connexion", connexion);
 
 // Route de connexion
 app.post("/login", (req, res, next) => {
@@ -66,8 +72,10 @@ app.post("/login", (req, res, next) => {
         .send(info.message || "Nom d’utilisateur ou mot de passe incorrect");
 
     req.login(user, (err) => {
-      if (err)
+      if (err) {
+        logger.error(err);
         return res.status(500).send("Erreur lors de la création de la session");
+      }
 
       res.json({ message: "Connexion réussie", user: user.nom_utilisateur });
       logger.info("Connexion réussie");
